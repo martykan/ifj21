59097a91ca4e11ef3bd7148b0536970d5b6318c6
Filip Štolfa <phillipstolfa@gmail.com>
2021-10-31 12:14:55 +0100

    Create tests for dynamic stirng

--- tests/unit/main.c
+++ tests/unit/main.c
@@ -1,21 +1,14 @@
 #include "../../lib/greatest.h"
 
-TEST x_should_equal_1(void) {
-  int x = 1;
 
-  ASSERT_EQ(1, x);
-
-  ASSERT_EQm("X doesn't equal 1!", 1, x);
-
-  PASS();
-}
-
-SUITE(the_suite) { RUN_TEST(x_should_equal_1); }
+SUITE_EXTERN(dynstr_basic_tests);
 
 GREATEST_MAIN_DEFS();
 
 int main(int argc, char **argv) {
   GREATEST_MAIN_BEGIN();
-  RUN_SUITE(the_suite);
+  RUN_SUITE(dynstr_basic_tests);
+
+
   GREATEST_MAIN_END();
 }
