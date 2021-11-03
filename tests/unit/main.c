7ecc27f4206ce764e755c49d9f668c6371854b8c
Filip Štolfa <phillipstolfa@gmail.com>
2021-11-03 02:12:42 +0100

    Create tests for scanner

--- tests/unit/main.c
+++ tests/unit/main.c
@@ -2,12 +2,17 @@
 
 
 SUITE_EXTERN(dynstr_basic_tests);
+SUITE_EXTERN(scanner_basic_tests);
+SUITE_EXTERN(scanner_input_file_tests);
 
 GREATEST_MAIN_DEFS();
 
 int main(int argc, char **argv) {
   GREATEST_MAIN_BEGIN();
+
   RUN_SUITE(dynstr_basic_tests);
+  RUN_SUITE(scanner_basic_tests);
+  RUN_SUITE(scanner_input_file_tests);
 
 
   GREATEST_MAIN_END();
