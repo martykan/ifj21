89781eff7080b8944fc34eff48f669e34d5c6709
patrikk0123 <patrik.korytar@gmail.com>
2021-11-19 11:32:13 +0100

    Added module for various functions

---
+++ src/other.c
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * @brief Generally useful functions
+ * @author Patrik Korytar
+ * @author Tomas Martykan
+ * @author Filip Stolfa
+ *
+ * TODO...
+ */
+
+#include "other.h"
+
+#include <stdlib.h>
+
+#include "errors.h"
+
+char* str_create_copy(char* str) {
+  char* copy = malloc(strlen(str) + 1);
+  if (!copy) {
+    error_set(EXITSTATUS_INTERNAL_ERROR);
+    return NULL;
+  }
+
+  strcpy(copy, str);
+
+  return copy;
+}
---
+++ src/other.h
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * @brief Generally useful functions API
+ * @author Patrik Korytar
+ * @author Tomas Martykan
+ * @author Filip Stolfa
+ *
+ * @section DESCRIPTION
+ *  TODO
+ *
+ * @section IMPLEMENTATION
+ *  TODO
+ */
+
+#ifndef __OTHER_H__
+#define __OTHER_H__
+
+char* str_create_copy(char* str);
+
+#endif // __OTHER_H__
