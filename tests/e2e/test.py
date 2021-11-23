2ccd153e0621df38737fa1ca38a64dc599928016
Filip Štolfa <phillipstolfa@gmail.com>
2021-11-24 00:24:53 +0100

    Implement comparing exit codes and outputs in e2e tests

--- tests/e2e/test.py
+++ tests/e2e/test.py
@@ -11,6 +11,8 @@ class TestRes:
     DIFF_EXIT_CODE = 3
     SKIPPED = 4
     INVALID_INPUT = 5
+    INTERNAL = 6
+    RUNTIME_ERROR = 7
 
     def __init__(self, type, msg="", ref_out="", out="", ref_ec=0, ec=0):
         self.type = type
@@ -40,22 +42,24 @@ def run_test(test_file: str, test_input_dir: str) -> TestRes:
     input_data_cmd = get_input_file(test_file, test_input_dir)
     input_data_cmd = f"<{input_data_cmd}" if len(input_data_cmd) > 0 else ""
 
+    try:
+        expected_rc = int(test_file[:test_file.index("_")])
+    except ValueError as e:
+        return TestRes(TestRes.INTERNAL, msg=f"Invalid test file name: {test_file}");
+
     ref_cmd = f"tl run {get_path(test_input_dir, test_file)}{input_data_cmd}"
     comp_cmd = f"../../ifj21 < {get_path(test_input_dir, test_file)}"
 
     try:
-        p = subprocess.Popen(
+        ref = subprocess.Popen(
             ref_cmd,
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             shell=True,
         )
-        out, err = p.communicate(timeout=3)
-        ref_rc = p.returncode
-        # if p.returncode != 0:
-        #     msg = f"Ref interpreter: Invalid input. rc={p.returncode}\n{err}"
-        #     return TestRes(TestRes.INVALID_INPUT, msg)
+        ref_out, ref_err = ref.communicate(timeout=3)
+        ref_rc = ref.returncode
 
         comp = subprocess.Popen(
             comp_cmd,
@@ -66,7 +70,6 @@ def run_test(test_file: str, test_input_dir: str) -> TestRes:
         )
 
         comp_out, comp_err = comp.communicate(timeout=3)
-        # print(comp_out.decode())
         comp_rc = comp.returncode
 
         comp_out_file = f"out_{test_file}"
@@ -74,8 +77,11 @@ def run_test(test_file: str, test_input_dir: str) -> TestRes:
         f.write(comp_out.decode())
         f.close()
 
+        if expected_rc != comp_rc:
+            return TestRes(TestRes.DIFF_EXIT_CODE, msg=f"Expected exit code doesn't match actual:\nexpected: {expected_rc}, got: {comp_rc}")
+
         # interpret generated code
-        if test_file == "hello.tl":
+        if expected_rc == 0:
             inter = subprocess.Popen(
                 f"./ic21int {comp_out_file}",
                 stdin=subprocess.PIPE,
@@ -84,15 +90,17 @@ def run_test(test_file: str, test_input_dir: str) -> TestRes:
                 shell=True,
             )
             inter_out, inter_err = inter.communicate(timeout=3)
-            print(inter_out.decode(), inter_err.decode())
+            if inter.returncode != 0:
+                return TestRes(TestRes.RUNTIME_ERROR, msg=f"Interpreter reported a runtime error: {inter.returncode}\n{inter_err.decode()}")
 
-        # TODO(filip): compare outputs
-        # TODO(filip): compare exit codes
-        # print(out.decode(), err.decode(), end="")
+            if inter_out.decode() != ref_out.decode():
+                return TestRes(TestRes.DIFF_OUT, msg=f"Outputs don't match:\nExpected:\n{ref_out.decode()}\n\nGot:\n{inter_out.decode()}")
+
     except subprocess.TimeoutExpired as e:
         return TestRes(TestRes.TIMEOUT, msg=str(e))
 
     else:
+        os.remove(get_path(test_input_dir, f"out_{test_file}"))
         return TestRes(TestRes.OK)
 
 
@@ -107,16 +115,19 @@ def print_test_suite_footer(num_run: int, num_failed: int, num_skipped: int):
 
 
 def print_test_fail(test_name: str, res: TestRes):
-    test_res_str = ["OK", "OUT_DIFF", "TIMEOUT"]
+
+    test_res_str = ["OK", "DIFF_OUT", "TIMEOUT", "DIFF_EXIT_CODE", "SKIPPED", "INVALID_INPUT", "INTERNAL", "RUNTIME_ERROR"]
     print(f"Fail - {test_name}: {test_res_str[res.type]}")
     if len(res.msg) != 0:
         print(res.msg)
-    if res.type == TestRes.DIFF_OUT:
-        print("Expected output:")
-        print(res.ref_out)
-        print("----------------------\n\n", end="")
-        print("Got output:")
-        print(res.out)
+    # if res.type == TestRes.DIFF_OUT:
+    #     print("Expected output:")
+    #     print(res.ref_out)
+    #     print("----------------------\n\n", end="")
+    #     print("Got output:")
+    #     print(res.out)
+
+    print("\n", end="")
 
 
 def print_test_success():
